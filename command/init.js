9efe379f159e2628f053b2adc24d3afc992db83b
Zyan <coderZyan@outlook.com>
2017-05-12 13:48:40 +0800

    0.0.3:基础功能实现

--- command/init.js
+++ command/init.js
@@ -1,38 +1,42 @@
 'use strict'
 const exec = require('child_process').exec
-const co = require('co')
-const prompt = require('co-prompt')
 const config = require('../templates')
-const chalk = require('chalk')
+const chalk = require('chalk'),
+  inquirer = require('inquirer');
 
 module.exports = () => {
- co(function *() {
-    // 处理用户输入
-      let tplName = yield prompt('Template name: ')
-      let projectName = yield prompt('Project name: ')
-      let gitUrl
-      let branch
 
-    if (!config.tpl[tplName]) {
-        console.log(chalk.red('\n × Template does not exit!'))
-        process.exit()
+  const questions = [{
+    type: 'input',
+    name: 'TemplateName',
+    message: '请输入模版名称',
+    default: 'demo'
+  }, {
+    name: 'projectName',
+    message: '请输入项目名称',
+    default: 'demo'
+  }]
+  inquirer.prompt(questions).then(function (answer) {
+    if (!config.tpl[answer.TemplateName]) {
+      console.log(chalk.red('\n × 模版不存在'))
+      process.exit()
     }
-    gitUrl = config.tpl[tplName].url
-    branch = config.tpl[tplName].branch
+    let gitUrl = config.tpl[answer.TemplateName].url,
+        projectName = answer.projectName;
 
     // git命令，远程拉取项目并自定义项目名
-    let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`
+    let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName}`
 
-    console.log(chalk.white('\n Start generating...'))
+    console.log(chalk.white('\n 开始生成...'))
 
     exec(cmdStr, (error, stdout, stderr) => {
       if (error) {
         console.log(error)
         process.exit()
       }
-      console.log(chalk.green('\n √ Generation completed!'))
+      console.log(chalk.green('\n √ 完成!'))
       console.log(`\n cd ${projectName} && npm install \n`)
       process.exit()
     })
-  })
+  });
 }
